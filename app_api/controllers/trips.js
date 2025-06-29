23abff889058b06ed6a9e52a0800e2c4963a32f0
Brad <bradleypeterson24@gmail.com>
2025-06-29 14:54:28 -0600

    module 6

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -3,47 +3,110 @@ const Trip = require('../models/travlr');
 const Model = mongoose.model('trips');
 
 //GET: /trips - all trips
-const tripsList = async(req, res) => {
-    const q = await Model   
+const tripsList = async (req, res) => {
+    const q = await Model
         .find({})
         .exec();
 
-        console.log(q);
+    console.log(q);
 
-    if(!q)
-    {
+    if (!q) {
         return res
-                .status(404)
-                .json(err);
+            .status(404)
+            .json(err);
     } else {
-        return res 
+        return res
             .status(200)
             .json(q);
     }
 };
 
 //GET: /trips/:tripCode - lists a single trip
-const tripsFindByCode = async(req, res) => {
-    const q = await Model   
-        .find({'code' : req.params.tripCode})
+const tripsFindByCode = async (req, res) => {
+    const q = await Model
+        .find({ 'code': req.params.tripCode })
         .exec();
 
-        console.log(q);
+    console.log(q);
 
-    if(!q)
-    {
+    if (!q) {
         return res
-                .status(404)
-                .json(err);
+            .status(404)
+            .json(err);
     } else {
-        return res 
+        return res
             .status(200)
             .json(q);
     }
 };
 
+// POST: /trips - Add new trip
+const tripsAddTrip = async (req, res) => {
+    const newtrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+    });
+
+    const q = await newtrip.save();
+
+    if (!q) {
+        //Database returned no data
+        return res
+            .status(400)
+            .json(err);
+    } else {
+        return res
+            .status(201)
+            .json(q);
+    }
+
+    //console.log(q);
+};
+
+//PUT: /trips/:tripCode - adds a new Trip
+const tripsUpdateTrip = async (req, res) => {
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+    if (!q) { // Database returned no data
+        return res
+            .status(400)
+            .json(err);
+    } else { // Return resulting updated trip
+        return res
+            .status(201)
+            .json(q);
+    }
+    // Uncomment the following line to show results of operation
+    // on the console
+    // console.log(q);
+};
+
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
